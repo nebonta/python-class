0cb2ca56221d883b1fd8cc8b856b5a291c3a472b
Nonso Nebonta <nebonta@gmail.com>
2025-09-16 05:47:48 +0100

    third commit

--- password_generator.py
+++ password_generator.py
@@ -9,15 +9,36 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# The Easy level
-password = ""
+# # The Easy level
+# password = ""
+# for char in range(0, nr_letters):
+#     password += random.choice(letters)
+
+# for char in range(0, nr_symbols):
+#     password += random.choice(symbols)
+
+# for char in range(0, nr_numbers):
+#     password += random.choice(numbers)
+
+# print(password)
+
+# The Hard Level
+password_list = []
 for char in range(0, nr_letters):
-    password += random.choice(letters)
+    password_list.append(random.choice(letters))
 
-for char in range(0, nr_symbols):
-    password += random.choice(symbols)
+for symbol in range(0, nr_symbols):
+    password_list.append(random.choice(symbols))
 
-for char in range(0, nr_numbers):
-    password += random.choice(numbers)
+for number in range(0, nr_numbers):
+    password_list.append(random.choice(numbers))
+    
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
 
-print(password)
+password = ""
+for char in password_list:
+    password += char
+    
+print(f"Your Password is: {password}")
