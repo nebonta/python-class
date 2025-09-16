9da75b74df2234702deace165375f6213c7907b1
Nonso Nebonta <nebonta@gmail.com>
2025-09-16 05:50:54 +0100

    The hard level

--- password_generator.py
+++ password_generator.py
@@ -1,6 +1,7 @@
 import random
 
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+           'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
@@ -9,18 +10,18 @@ nr_letters = int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-# # The Easy level
-# password = ""
-# for char in range(0, nr_letters):
-#     password += random.choice(letters)
+# The Easy level
+password = ""
+for char in range(0, nr_letters):
+    password += random.choice(letters)
 
-# for char in range(0, nr_symbols):
-#     password += random.choice(symbols)
+for char in range(0, nr_symbols):
+    password += random.choice(symbols)
 
-# for char in range(0, nr_numbers):
-#     password += random.choice(numbers)
+for char in range(0, nr_numbers):
+    password += random.choice(numbers)
 
-# print(password)
+print(password)
 
 # The Hard Level
 password_list = []
@@ -32,7 +33,7 @@ for symbol in range(0, nr_symbols):
 
 for number in range(0, nr_numbers):
     password_list.append(random.choice(numbers))
-    
+
 print(password_list)
 random.shuffle(password_list)
 print(password_list)
@@ -40,5 +41,5 @@ print(password_list)
 password = ""
 for char in password_list:
     password += char
-    
+
 print(f"Your Password is: {password}")
